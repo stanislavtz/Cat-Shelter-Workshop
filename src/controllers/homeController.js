03e2f3b81ebadc700df84676932a923e1aa4a204
Tzekov <stanislavtz@gmail.com>
2022-04-26 11:29:21 +0300

    Update main view

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -3,7 +3,7 @@ const db = require('../utils/db.json');
 const cats = Object.values(db.cats);
 
 function homePage(req, res) {
-    res.render('home/index', { cats });
+    res.render('home/index', { cats, homepage: true });
 }
 
 module.exports = homePage;
