0b77b9511f2960c53271ed2ec0b61fe4bea7fa03
Tzekov <stanislavtz@gmail.com>
2022-04-26 10:27:18 +0300

    Update homeController.js
    Add homepage controller

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,1 +1,9 @@
-//TO DO...
+const db = require('../utils/db.json');
+
+const cats = Object.values(db.cats);
+
+function homePage(req, res) {
+    res.render('home/index', { cats });
+}
+
+module.exports = homePage;
