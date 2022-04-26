7c8a19c82f17eaef7ef1718f946316941bd1a226
Tzekov <stanislavtz@gmail.com>
2022-04-26 11:03:22 +0300

    Rename add cat and breed controllers

--- src/routers/catsRouter.js
+++ src/routers/catsRouter.js
@@ -1,14 +1,17 @@
 const router = require('express').Router();
+const db = require('../utils/db.json');
 
-router.get('/add-breed', addBreeds);
-router.get('/add-cat', addCats);
+router.get('/add-breed', addBreed);
+router.get('/add-cat', addCat);
 
-function addBreeds(req, res) {
+function addBreed(req, res) {
     res.render('addBreed');
 }
 
-function addCats(req, res) {
-    res.render('addCat');
+function addCat
+(req, res) {
+    const breeds = db.breeds;
+    res.render('addCat', { breeds });
 }
 
 module.exports = router;
