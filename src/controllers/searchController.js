9c2c32522651133e12054d19116599d6ea4a450b
Tzekov <stanislavtz@gmail.com>
2022-04-27 14:30:23 +0300

    Remove search controller

--- src/controllers/searchController.js
+++
@@ -1,9 +0,0 @@
-const db = require('../utils/db.json');
-
-const searchBar = (req, res) => {
-    const filteredCats = Object.values(db.cats).filter(cat => cat.breed.includes(req.query.search.toLowerCase()));
-
-    res.render('home/index', { cats: filteredCats, homepage: true })
-}
-
-module.exports = searchBar;
