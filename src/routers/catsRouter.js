9079a337f3e0f0a7852a70ff8978036ba25c153b
Tzekov <46439753+stanislavtz@users.noreply.github.com>
2022-04-29 09:13:00 +0300

    Update post emitter add-cat callback

--- src/routers/catsRouter.js
+++ src/routers/catsRouter.js
@@ -1,20 +1,23 @@
 const router = require('express').Router();
-const db = require('../utils/db.json');
 const uniqid = require('uniqid');
 
 const addCat = require('../controllers/catController');
 const addBreed = require('../controllers/breedController');
 const Breed = require('../models/Breed');
+const Cat = require('../models/Cat.js');
 
 router.get('/add-cat', addCat);
 router.get('/add-breed', addBreed);
 
 router.post('/add-cat', (req, res) => {
     const _id = uniqid();
-    const cat = {_id, ...req.body }
-    // console.log('ASD',req.locals.cats);
+    const { name, breed, description, imageUrl } = req.body;
+    const cat = new Cat(_id, name, breed, description, imageUrl);
 
-    // req.cats[_id] = cat;
+    Cat.save(cat);
+
+    console.log('ASD', cat);
+
     res.redirect('/');
 });
 
