1c308357f776f7d4a7d409e26d95ca6860abf8a1
Tzekov <46439753+stanislavtz@users.noreply.github.com>
2022-04-29 13:06:45 +0300

    Add renderEditCatPage and delete cat functionality

--- src/controllers/catController.js
+++ src/controllers/catController.js
@@ -1,9 +1,18 @@
 const router = require('express').Router();
-const { addCat, renderAddCatPage, renderEditCatPage } = require('../services/catsService');
+const { 
+    addCat, 
+    editCat, 
+    deleteCat, 
+    renderAddCatPage, 
+    renderEditCatPage 
+} = require('../services/catsService');
 
 router.get('/add-cat', renderAddCatPage);
 router.post('/add-cat', addCat);
 
-router.get('/cat/:_id/edit', renderEditCatPage);
+router.get('/cat/:id/edit', renderEditCatPage);
+router.post('/cat/:id/edit', editCat);
+
+router.get('/cat/:id/shelter', deleteCat);
 
 module.exports = router;
--- src/services/catsService.js
+++ src/services/catsService.js
@@ -2,6 +2,7 @@ const uniqId = require('uniqid');
 const Breed = require("../models/Breed");
 const Cat = require("../models/Cat");
 
+let cat = {};
 
 const renderAddCatPage = (req, res) => {
     const breeds = Breed.getAll();
@@ -9,27 +10,35 @@ const renderAddCatPage = (req, res) => {
 }
 
 const renderEditCatPage = (req, res) => {
-    console.log(req.params)
-    const cat = Cat.getById(req.params._id);
+    cat = { ...Cat.getById(req.params.id) };
     const breeds = Breed.getAll().filter(b => b.name !== cat.breed);
-    
-    res.render('editCat', {cat, breeds});
+
+    res.render('editCat', { cat, breeds });
 }
 
 const addCat = (req, res) => {
     const id = uniqId();
     const { name, breed, description, imageUrl } = req.body;
-    
+
     Cat.save(id, name, breed, description, imageUrl);
     res.redirect('/');
 }
 
 const editCat = (req, res) => {
-    console.log(req.params)
+    console.log(cat);
+    console.log(req.body);
+    res.redirect('/');
+}
+
+const deleteCat = (req, res) => {
+    Cat.removeById(req.params.id);
+    res.redirect('/');
 }
 
 module.exports = {
     addCat,
+    editCat,
+    deleteCat,
     renderAddCatPage,
     renderEditCatPage,
 }
