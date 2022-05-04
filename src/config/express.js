18b24bbf1ff188b44e15727e4a7be6e1c89ad3f3
Tzekov <stanislavtz@gmail.com>
2022-05-04 11:32:56 +0300

    Update index and express config

--- src/config/express.js
+++ src/config/express.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const path = require('path');
-
 const router = require('../routes');
-const { port } = require('../utils/constants');
 
 function expressConfig(app) {
     app.use(express.static(path.resolve(__dirname, '../public')));
     app.use(express.urlencoded({ extended: true }));
     app.use(router);
-
-    app.listen(port, console.log.bind(console, `Server is running on http://localhost:${port}`));
 }
 
 module.exports = expressConfig;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 
+const { port } = require('./utils/constants');
+
 const app = express();
 
 require('./config/express')(app);
 require('./config/handlebars')(app);
+
+app.listen(port, console.log.bind(console, `Server is running on http://localhost:${port}`));
