8be9fb4a7b13deec5205157dfd9e97b2a105c446
Tzekov <stanislavtz@gmail.com>
2022-05-04 11:20:04 +0300

    Export cats and breeds collections

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,46 @@
 exports.port = 8000;
+
+exports.cats = {
+    asd345sdf01A: {
+        _id: 'asd345sdf01A',
+        name: 'Blacky',
+        breed: 'Bombay',
+        description: 'Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.',
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/03/27/13/16/cat-694730_1280.jpg'
+    },
+    asd345sdf02B: {
+        _id: 'asd345sdf02B',
+        name: 'Tigerony',
+        breed: 'Tiger-Cat',
+        description: 'Aggressive wild cat',
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/06/19/14/20/cat-814952_1280.jpg'
+    },
+    asd345sdf03C: {
+        _id: 'asd345sdf03C',
+        name: 'Snow Queen',
+        breed: 'Persian',
+        description: 'Smart and brave cat.',
+        imageUrl: 'https://cdn.pixabay.com/photo/2018/08/08/05/12/cat-3591348_1280.jpg'
+    },
+    asd345sdf04D: {
+        _id: 'asd345sdf04D',
+        name: 'Puffy',
+        breed: 'Angora',
+        description: 'Cute funny cat, with soft and warm fur',
+        imageUrl: 'https://cdn.pixabay.com/photo/2014/04/13/20/49/cat-323262_1280.jpg'
+    },
+    asd345sdfASD1: {
+        _id: 'asd345sdfASD1',
+        name: 'Mussy',
+        breed: 'Angora',
+        description: 'Little cute kitty cat',
+        imageUrl: 'https://www.petmd.com/sites/default/files/small-kitten-walking-towards_127900829_0.jpg'
+    }
+}
+
+exports.breeds = [
+    { name: 'Bombay' },
+    { name: 'Angora' },
+    { name: 'Persian' },
+    { name: 'Tiger-Cat' },
+];
